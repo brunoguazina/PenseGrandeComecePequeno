7e41b3577f3a688068401a845e608864f983a2a4
brunoguazina <bruno@guazina.com>
2015-03-27 14:48:37 -0400

    ajustes na estrutura para publicaco

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,23 @@
-/* jshint devel:true */
-$("#email").focus();
+
+$("#mce-EMAIL").focus();
+
+
+$("#mc-embedded-subscribe-form").submit(function(){
+	var email = $("#mce-EMAIL").val();
+	if(email != "") {
+		var filtro = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+         
+		if(filtro.test(email)) {
+			$("#mce-EMAIL").val("email enviado com sucesso...");
+	   		return true;
+		}
+		else {
+			alert("Este endereço de email não é válido!");
+			return false;
+		}
+	}
+	else {
+		$("#mce-EMAIL").val("Digite um email!");
+		return false;
+	}
+});
